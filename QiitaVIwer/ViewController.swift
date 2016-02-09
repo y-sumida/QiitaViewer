6e431d47d9ffa53214975e9aeb2a8a28588cdf93
Yuki Sumida <y.sumida.0201@gmail.com>
2016-02-09 09:29:10 +0900

    新着記事用のテーブルレイアウトを作成

--- QiitaVIwer/ViewController.swift
+++ QiitaVIwer/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let table = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        title = "新着記事"
+
+        table.frame = view.frame
+        view.addSubview(table)
+
     }
 
     override func didReceiveMemoryWarning() {
